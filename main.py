be0113891159fd93963f142b0ccfb7e73daa5422
Shrishailya Abhay Keskar <130174278+kshree2513@users.noreply.github.com>
2023-11-15 19:54:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from Scoreboard import Scoreboard
 import time
 
+#
+# class line(Turtle):
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.color("white")
+#         self.goto(0, 300)
+#         for i in range(30):
+#             if i % 2 == 0:
+#                 self.penup()
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-r_paddle = Paddle((350,0))
-l_paddle = Paddle((-350,0))
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+score = Scoreboard()
 screen.listen()
-screen.onkey(r_paddle.Up,"Up")
-screen.onkey(r_paddle.Down,"Down")
-screen.onkey(l_paddle.Up,"w")
-screen.onkey(l_paddle.Down,"s")
+screen.onkey(r_paddle.Up, "Up")
+screen.onkey(r_paddle.Down, "Down")
+screen.onkey(l_paddle.Up, "w")
+screen.onkey(l_paddle.Down, "s")
 
 ball = Ball()
-
 is_game_on = True 
 while is_game_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
-    if ball.ycor() > 280 or ball.ycor() < -280 :
+    if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
-    if (ball.distance(r_paddle) < 70 and ball.xcor() > 340) :
+    if ball.distance(r_paddle) < 70 and ball.xcor() > 320 or ball.distance(l_paddle) < 70 and ball.xcor() < -320:
         ball.bounce_x()
-    if (ball.distance(l_paddle) < 70 and ball.xcor() < -340) :
-        ball.bounce_x()
+    if ball.xcor() > 380:
+        ball.restart()
+        score.l_point()
+    if ball.xcor() < -380:
+        ball.restart()
+        score.r_point()
 
 screen.exitonclick()
