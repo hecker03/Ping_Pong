2ee35d9a2d6b7190a26016810fd689c86c690700
Shrishailya Abhay Keskar <130174278+kshree2513@users.noreply.github.com>
2023-11-15 19:54:19 +0530

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 class Paddle(Turtle):
 
     def __init__(self, position):
@@ -12,8 +13,10 @@ class Paddle(Turtle):
 
     def Up(self):
         new_y = self.ycor() + 20 
-        self.goto(self.xcor(),new_y)
+        self.goto(self.xcor(), new_y)
 
     def Down(self):
         new_y = self.ycor() - 20 
-        self.goto(self.xcor(),new_y)
+        self.goto(self.xcor(), new_y)
+
+
